ecfacf8f1a6bcfed8226afc2a0f12ff70e54b751
AdiSAnkolli <59432457+AdiSAnkolli@users.noreply.github.com>
2020-06-04 16:21:16 +0530

    Update Read_Images_Video.py

--- Read_Images_Video.py
+++ Read_Images_Video.py
@@ -2,7 +2,7 @@
 
 import cv2
 # LOAD AN IMAGE USING 'IMREAD'
-img = cv2.imread("Resources/lena.png")
+img = cv2.imread("Resources/lena.png")  # Path to the image
 # DISPLAY
 cv2.imshow("Lena Soderberg",img)
 cv2.waitKey(0)
@@ -12,7 +12,7 @@ cv2.waitKey(0)
 import cv2
 frameWidth = 640
 frameHeight = 480
-cap = cv2.VideoCapture("Resources/test_ video.mp4")
+cap = cv2.VideoCapture("Resources/test_ video.mp4")   # Path to the video
 while True:
     success, img = cap.read()
     img = cv2.resize(img, (frameWidth, frameHeight))
@@ -25,7 +25,7 @@ while True:
 import cv2
 frameWidth = 640
 frameHeight = 480
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # Default code '0', i.e for web cam
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)
